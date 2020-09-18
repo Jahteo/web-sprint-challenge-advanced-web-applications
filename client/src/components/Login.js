36da7c9e180e89f6e8c3f8f5bd0b45440d294e55
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-18 08:26:35 -0700

    setup form, AxiosWithAuth is next

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom"
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+  });
+  const history = useHistory();
+
+  const handleChanges = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form >
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={user.username}
+          onChange={handleChanges}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={user.password}
+          onChange={handleChanges}
+        />
+        <button>Login</button>
+
+      </form>
+
     </>
   );
 };
